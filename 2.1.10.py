da2a92f82083603df569b854916a7b96c5990512
Tuansan2411 <112991603+Tuansan2411@users.noreply.github.com>
2022-09-29 09:03:11 +0700

    Add files via upload

---
+++ 2.1.10.py
@@ -0,0 +1,5 @@
+a=" hi i am pyhon programmer"
+b=a.split()
+print(b)
+c=" ".join(b)
+print(c)
---
+++ 5.1.2.py
@@ -0,0 +1,9 @@
+import datetime as dt
+format = '%Y-%m-%dT%H:%M:%S'
+t1=dt.datetime.strptime('2008-10-12T14:45:52',format)
+print('Mouth' + str(t1.month))
+print('Minute'+str(t1.minute))
+print('Second'+str(t1.second))
+t2=dt.datetime.now()
+diff=t2-t1
+print('How many days diffference?'+str(diff.days))
